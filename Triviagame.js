a13d4f11db961600d858aead43988437480fdd66
Patrick Mahon <p.mahon0628@gmail.com>
2019-11-26 22:13:39 -0500

    Schitts Creek Trivia

--- Triviagame.js
+++ Triviagame.js
@@ -1,164 +1,101 @@
-var questions = [{
-    ques: "What year did the Battle of Hastings Occur?",
-    ans: ["1115", "1066", "1123", "987"],
-    name: "Hastings",
-    correct: "1066",
-    divClass: ".Hastings"
-},
-{
-    ques: "What river did the U.S and Britsh forces first cross to get into Germany in WW2?",
-    ans: ["Danube", "Elbe", "Rhine", "Seine"],
-    name: "Rhine",
-    correct: "Rhine",
-    divClass: ".River"
-},
-{
-    ques: "Who was king of England during the American Revolution?",
-    ans: ["Henrey VII", "George VI", "George III", "George II"],
-    name: "Kings",
-    correct: "George III",
-    divClass: ".Kings"
-},
-{
-    ques: "Who was the last king of Russia and when did he die?",
-    ans: ["Nicholas II, 1918", "Charles III, 1917", "Nicolas II, 1917", "Ivan IV, 1914"],
-    name: "Russia",
-    correct: "Nicholas II, 1917",
-    divClass: ".Russia"
-},
-{
-    ques: "Who was the first secretary of treasury",
-    ans: ["Thomas Jefferson", "Ben Franklin", "Alexander Hamilton", "John Adams"],
-    name: "Hamilton",
-    correct: "Alexander Hamilton",
-    divClass: ".Hamilton"
-},
-{
-    ques: "This person became president after Lincoln",
-    ans: ["Ulyses Grant", "Franklin Pierce", "James Buchanan", "Andrew Johnson"],
-    name: "Johnson",
-    correct: "Andrew Johnson",
-    divClass: ".Johnson"
-},
-{
-    ques: "In 1796, this scientist discovered the vaccaniation against Polio",
-    ans: ["Michael Faraday", "Edward Jenner", "Marie Cure", "Neils Bohr"],
-    name: "Jenner",
-    correct: "Edward Jenner",
-    divClass: ".Jenner"
-},
-{
-    ques: "What was the first battle in WW1?",
-    ans: ["Battle of the Somme", "Battle of Mullhouse", "First Battle of the Marne", "Battle of the Lorraine"],
-    name: "Mullhouse",
-    correct: "Battle of Mullhouse",
-    divClass: ".Mullhouse"
-},
-{
-    ques: "Wnat ship was the Japanese surrender signed on at the end of WW2?",
-    ans: ["Missouri", "Iowa", "Yorktown", "Minnesota"],
-    name: "Surrender",
-    correct: "Missouri",
-    divClass: ".Surrender"
-},
-{
-    ques: "What year was the world wide web invented?",
-    ans: ["1989", "1988", "1990", "1987"],
-    name: "Year",
-    correct: "1989",
-    divClass: ".Year"
-}
-] // end questions object
+$(function (){
+    const game= [
+        {
+            question: 'Who was the Roses business manager? :',
+            answers: ['Mike', 'James', 'Eli', 'Patrick' ],
+            correct: 2,
+            image: '',
+        },
+        {
+            question: 'This is the the town logo:',
+            answers: ['Welcome Home', 'Happy To See You', 'Welcome Back', 'Where Everyone Fits In' ],
+            correct: 3,
+            image: '',
+        },
+        {
+            question: 'WHat TV show did Moira star in? :',
+            answers: ['Sunrise Bay', 'All My Sisters', 'Ocean View', 'Our Family' ],
+            correct: 0,
+            image: '',
+        },
+        {
+            question: 'What store was before Rose Apothecary:',
+            answers: ['Drugstore', 'Bookstore', 'General Store', 'Post Office' ],
+            correct: 2,
+            image: '',
+        },
+        {
+            question: 'Who sings Simply The Best? :',
+            answers: ['Etta James', 'Tina Turner', 'Whitney Houston', 'Celine Dion' ],
+            correct: 1,
+            image: '',
+        },
+        {
+            question: 'Who was Alexis ex-boyfreind?: ',
+            answers: ['Mike', 'Miguel', 'Tim', 'Stavros' ],
+            correct: 3,
+            image: '',
+        },
+        {
+            question: 'Who was the Roses business manager:',
+            answers: ['Mike', 'James', 'Eli', 'Patrick' ],
+            correct: 2
+            image: '',
+        },
+        {
+            question: 'Who was the Roses business manager:',
+            answers: ['Mike', 'James', 'Eli', 'Patrick' ],
+            correct: 2
+            image: '',
+        },
+        {
+            question: 'Who does David stay with when he runs away? :',
+            answers: ['Bob', 'The Amish', 'a family in Elmdale', 'Noone' ],
+            correct: 1,
+            image: '',
+        },
+        {
+            question: 'Who does Patrick work with when he meets David?:',
+            answers: ['Bob', 'Roland', 'Ray', 'Ronnie'],
+            correct: 2,
+            image: '',
+        }],
 
-var labels = ["first", "second", "third", "forth"];
 
-// click to start then display quesions
-var startGame = $(".start-btn").on('click', function() {
-$(this).parent().hide();
-$('.container').show();
-countdown(60);
-questionDisplay();
-});
+// Variables:
+let timer = 10;
+let timerId;
+let timerGo = false;
+let unanswered = 0;
+let right = 0;
+let wrong = 0;
+let currentQuestion = 0;
+let userGuess = '';
+let end = game.lenghth;
 
-console.log("start-btn");
-console.log(this)
+$('.resst').hide();
 
-// function for displaying questions
-var questionDisplay = function() {
-$(".questions :not('#sub-but')").empty();
-for (var i = 0; i < 10; i++) {
-$('.questions').prepend('<div class="' + questions[i].name + '"></div>');
-$(questions[j].divClass).append('<div class ="ques-title">' + questions[i].ques + '</div>');
-for (var i = 0; i <= 3; i++) {
-    $(questions[j].divClass).append('<input type="radio"  name="' + questions[i].name + '" value="' + questions[i].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
-}
-$('.questions').prepend('<hr/>');
-}
-}
-
-var countdown = function(seconds) {
-
-var timer = setInterval(function() {
-seconds = seconds - 1;
-$("#time-remain").html(seconds);
-
-if (seconds <= 0) {
-    $('.container').fadeOut();
-    var correctAnswers = 0;
-    var wrongAnswers = 0;
-    var unAnswered = 0;
-
-    
-    for (var i = 0; i < 10; i++) {
-
-        if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
-
-            correct++;
-            console.log("this is correct! number:" + i)
-        } else {
-            wrongAnswers++;
-            console.log("this is wrong! number:" + i)
-        };
+const startTimer = () => {
+    if(!timerGo) {
+        timerId = setInterval(countdown, 1000);
+        timerGo = true;
     }
-    $('#correctTimesUp').append(correctAnswers);
-     $('#wrongTimesUp').append(wrongAnswers);
-    $('#timesUp').fadeIn(1000).show();
-
-    
-    clearInterval(timer);
-    return;
 }
-}, 1000);
+
+const countdown = () => {
+    $('.timer').html(`<h3>Go Faster! ${timer}</h3>`)
+    console.log(timer)
+
+    if (timer < 0){
+        unanswered++;
+        stopTimer();
+        $('.answers').empty();
+        $('.questions').empty();
+        $('.timer').html('You took to long.  The correct answer was ' + game[currentQuestion].answers[game[currentQuestion]);
+    ]currentQuestion++;
+    setTimeout(displayQuestion, 3000);
+    }
+}
 
 
-$('#Submit').on('click', function() {
-clearInterval(timer);
-})
-};
 
-
-
-var gradeQuiz = $('#Submit').on('click', function() {
-
-var correctAnswers = 0;
-var wrongAnswers = 0;
-var unAnswered = 0;
-
-
-for (var i = 0; i < 10; i++) {
-
-if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
-
-    correctAnswers++;
-} else {
-    wrongAnswers++;
-};
-};
-
-countdown();
-$('.container').fadeOut(500);
-$('#answerScreen').show();
-$('#correctScreen').append(correctAnswers);
-$('#wrongScreen').append(wrongAnswers);
-
-}); 
